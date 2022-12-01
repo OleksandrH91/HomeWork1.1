948d020d1940e3c5fea9834770160373895647e9
OleksandrH91 <116737899+OleksandrH91@users.noreply.github.com>
2022-12-01 20:27:06 +0200

    Data structures!

--- Task7/Main.java
+++ Task7/Main.java
@@ -1,32 +1,26 @@
 package Collections.Task7;
 
-import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
         int[] array = new int[200_000];
-        fillArray(array);
 
+        fillArray(array);
         SortClass.bubbleSort(array);
 
         fillArray(array);
-
         SortClass.selectionSort(array);
 
         fillArray(array);
-
         SortClass.quickSort(array) ;
 
         fillArray(array);
-
         SortClass.insertSort(array);
 
         fillArray(array);
-
-        System.out.println(Arrays.toString(array));
         SortClass.shuttleSort(array);
-        System.out.println(Arrays.toString(array));
+
     }
 
     public static void fillArray(int[] array) {
